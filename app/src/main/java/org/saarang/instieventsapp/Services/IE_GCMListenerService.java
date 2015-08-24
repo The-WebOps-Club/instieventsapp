e3cda775845d9b72d51a1d103b8651e22f04004e
kevin_selva_prasanna <kevinselvaprasanna@gmail.com>
2015-08-24 21:02:56 +0530

    GCM Services added

---
+++ app/src/main/java/org/saarang/instieventsapp/Services/IE_GCMListenerService.java
@@ -0,0 +1,21 @@
+package org.saarang.instieventsapp.Services;
+
+
+/**
+ * Created by kevin selva prasanna on 24-Aug-15.
+ */
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+public class IE_GCMListenerService extends GcmListenerService{
+}
---
+++ app/src/main/java/org/saarang/instieventsapp/Utils/AppConstants.java
@@ -0,0 +1,8 @@
+package org.saarang.instieventsapp.Utils;
+
+/**
+ * Created by kevin selva prasanna on 24-Aug-15.
+ */
+public class AppConstants {
+    public static String GCM_SENDER_ID = "0000000000";          //dummy id//
+}
